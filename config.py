ae6d5a18a3c26f9ebe5e5744ad823882cbc8967d
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-11 11:31:55 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    # The Telegram API things
+    APP_ID = int(os.environ.get("APP_ID", 12345))
+    API_HASH = os.environ.get("API_HASH")
+    # Get these values from my.telegram.org
+    # Array to store users who are authorized to use the bot
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    # Banned Unwanted Members..
+    BANNED_USERS = []
+    # the download location, where the HTTP Server runs
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
+    # https://t.me/hevcbay/951
+    OUO_IO_API_KEY = ""
+    # maximum message length in Telegram
+    MAX_MESSAGE_LENGTH = 4096
