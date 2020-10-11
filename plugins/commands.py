6d21ff479f550a6f4abf76edc82c01bfbe556d9a
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-11 12:33:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,2 +1,16 @@
 import pyrogram
+
 from pyrogram import Filters, InlineKeyboardMarkup, InlineKeyboardButton
+from translation import Translation
+
+@Client.on_message(Filters.command(["start"]))
+async def start(c, m):
+    await c.send_message(chat_id=m.chat.id,
+                         text=Translation.START.format(m.from_user.first_name),
+                         reply_to_message=m.message_id)
+
+@Client.on_message(Filters.command(["help"]))
+async def help(c, m):
+    await c.send_message(chat_id=m.chat.id,
+                         text=Translation.HELP,
+                         reply_to_message=m.message_id)
