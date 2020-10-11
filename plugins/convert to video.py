9440d5d6f1d9b03dda0e6f9ae9376b7cae3cd366
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-11 15:51:17 +0530

    Update and rename Convert.py to convert to video.py

---
+++ plugins/convert to video.py
@@ -0,0 +1,39 @@
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import pyrogram
+import time
+from pyrogram import Client, Filters 
+from Tools.Download import download
+from Tools.progress import progress_for_pyrogram
+
+@Client.on_message(Filters.command(["converttovideo"]))
+async def video(c, m):
+      await download(c, m)
+      logger.info("Downloading")
+
+      time = time.time()
+      await c.send_video(
+                chat_id=m.chat.id,
+                video=media_location,
+                duration=duration,
+                width=width,
+                height=height,
+                supports_streaming=True,
+                thumb=thumb_image_path,
+                reply_to_message_id=m.reply_to_message.message_id,
+                progress=progress_for_pyrogram,
+                progress_args=(
+                    "Upload Status:",
+                    send,
+                    time
+                )
+      )
+      try:
+          os.remove(media_location)
+      except:
+          pass
+      await send.edit_message_text(Translation.UPLOAD_COMPLETE)
