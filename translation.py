b4c1d9fddb0285cff24239658fa9c34f445af794
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-11 11:43:04 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,11 @@
+class Translation(object):
+      START = """Hello {}
+
+I am a converter clone of [Convert Ns Bot](https://telegram.dog/convert_Ns_bot) by {}
+
+I can convert file to video or video to file with custom thumbnail support"""
+      HELP = """Hey you need help 🤔
+
+1. Send me the telegram file or video which you wanted to convert.
+
+2. Use command /converttofile for converting into file
