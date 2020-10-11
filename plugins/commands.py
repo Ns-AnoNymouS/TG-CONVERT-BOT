f1a2936bf3758430d74f8ae730e1f997e7dc3fb1
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-11 14:37:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 import pyrogram
 
-from pyrogram import Filters, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import Filters
 from translation import Translation
 
 
@@ -16,7 +16,7 @@ async def start(c, m):
     await c.send_message(chat_id=m.chat.id,
                          text=Translation.START.format(m.from_user.first_name),
                          reply_to_message=m.message_id)
-    logger.info(f"{m.from_user.first_name}")
+    logger.info(f"{m.from_user.first_name} used start command")
 
 
 
@@ -25,4 +25,12 @@ async def help(c, m):
 
     await c.send_message(chat_id=m.chat.id,
                          text=Translation.HELP,
+                         reply_to_message=m.message_id)
+
+
+@Client.on_message(Filters.command(["about"]))
+async def about(c, m):
+
+    await c.send_message(chat_id=m.chat.id,
+                         text=Translation.ABOUT,
                          reply_to_message=m.message_id)
