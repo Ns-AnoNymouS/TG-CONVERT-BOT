dc30f8715e1466b5da3e06c7b44d6b947024d842
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-11 11:52:42 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -8,4 +8,12 @@ I can convert file to video or video to file with custom thumbnail support"""
 
 1. Send me the telegram file or video which you wanted to convert.
 
-2. Use command /converttofile for converting into file
+2. Send me the thumbnail(photo) optional.
+
+3. Reply to video /converttofile for converting into file.
+
+4. Reply to file /converttovideo for converting into video."""
+
+      ABOUT = """
+
+"""
