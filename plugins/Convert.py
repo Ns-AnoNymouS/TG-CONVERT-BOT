6311529ade1cf6c7d1ecb9a13c65970a55a0edbd
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-11 15:41:51 +0530

    Update Convert.py

--- plugins/Convert.py
+++ plugins/Convert.py
@@ -1,1 +1,8 @@
+import pyrogram
+import time
+from pyrogram import Client, Filters 
+from Tools.Download import Download
 
+
+@Client.on_message(Filters.command(["converttovideo"]))
+async def video(c, m):
