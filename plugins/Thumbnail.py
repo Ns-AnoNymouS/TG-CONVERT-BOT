26b9c63ddcc30e0fb2992569d35168a07f8629a7
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-11 15:32:32 +0530

    Create Thumbnail.py

---
+++ plugins/Thumbnail.py
@@ -0,0 +1,73 @@
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import numpy
+import os
+from PIL import Image
+import time
+
+from config import Config
+
+from translation import Translation
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+from pyrogram import Client, Filters 
+
+@Client.on_message(Filters.photo)
+async def save_photo(bot, update):
+    if update.from_user.id in Config.BANNED_USERS:
+        await bot.delete_messages(
+            chat_id=update.chat.id,
+            message_ids=update.message_id,
+            revoke=True
+        )
+        return
+    TRChatBase(update.from_user.id, update.text, "save_photo")
+    if update.media_group_id is not None:
+        # album is sent
+        download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + "/" + str(update.media_group_id) + "/"
+        # create download directory, if not exist
+        if not os.path.isdir(download_location):
+            os.makedirs(download_location)
+        await bot.download_media(
+            message=update,
+            file_name=download_location
+        )
+    else:
+        # received single photo
+        download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+        await bot.download_media(
+            message=update,
+            file_name=download_location
+        )
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.SAVED_CUSTOM_THUMB_NAIL,
+            reply_to_message_id=update.message_id
+        )
+
+
+@Client.on_message(Filters.command(["deletethumbnail"]))
+async def delete_thumbnail(bot, update):
+    if update.from_user.id in Config.BANNED_USERS:
+        await bot.delete_messages(
+            chat_id=update.chat.id,
+            message_ids=update.message_id,
+            revoke=True
+        )
+        return
+    TRChatBase(update.from_user.id, update.text, "deletethumbnail")
+    download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id)
+    try:
+        os.remove(download_location + ".jpg")
+        # os.remove(download_location + ".json")
+    except:
+        pass
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.DEL_ETED_CUSTOM_THUMB_NAIL,
+        reply_to_message_id=update.message_id
+    )
