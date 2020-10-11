26feb345b7b96657644851b13f655aaae4957cef
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-11 11:36:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ if __name__ == "__main__" :
         root="plugins"
     )
     app = Client(
-        "Convert Bot",
+        "CONVERTBOT",
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
