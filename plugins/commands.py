0dd8413504a39f470252067a7857ffde434ab4b9
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-12 08:36:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -9,13 +9,13 @@ from pyrogram import Client, Filters, InlineKeyboardButton, InlineKeyboardMarkup
 from translation import Translation
 from Tools.Download import download
 
-my_father = "tg://user?id={}".format(Config.USER_ID)
+my_father = "https://t.me/{}".format(Config.USER_NAME[1:])
 support = "https://telegram.dog/Ns_Bot_supporters"
 @Client.on_message(Filters.command(["start"]))
 async def start(c, m):
 
     await c.send_message(chat_id=m.chat.id,
-                         text=Translation.START.format(m.from_user.first_name, Config.OWNER_NAME),
+                         text=Translation.START.format(m.from_user.first_name, Config.USER_NAME),
                          reply_to_message_id=m.message_id,
                          reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("My Father 👨‍💻", url=my_father), InlineKeyboardButton("📌Support channel", url=support)]]))
     logger.info(f"{m.from_user.first_name} used start command")
