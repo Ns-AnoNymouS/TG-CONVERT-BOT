f3c3da1e41a6e72488fc8707387031a8534c71f9
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-12 06:50:31 +0530

    Create upload.py

---
+++ Tools/upload.py
@@ -0,0 +1,28 @@
+import os
+import pyrogram
+from Tools.progress import progress_for_pyrogram
+
+async def upload_video(c, m, send, media_location, thumb_image_path):
+      await send.edit(Translation.UPLOAD_START)
+      c_time = time.time()
+      await c.send_video(
+                chat_id=m.chat.id,
+                video=media_location,
+                duration=duration,
+                width=width,
+                height=height,
+                supports_streaming=True,
+                thumb=thumb_image_path,
+                reply_to_message_id=m.reply_to_message.message_id,
+                progress=progress_for_pyrogram,
+                progress_args=(
+                    "Upload Status:",
+                    send,
+                    c_time
+                )
+      )
+      try:
+          os.remove(media_location)
+      except:
+          pass
+      await send.edit_message_text(Translation.UPLOAD_COMPLETE)
