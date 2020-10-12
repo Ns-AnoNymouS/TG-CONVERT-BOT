90febc7ad8f35e95a459886d34abc011faa982c1
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-12 08:37:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ class Config(object):
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
     APP_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
-    OWNER_NAME = os.environ.get("OWNER_NAME", "")
-    USER_ID = os.environ.get("USER_ID", "")
+    USER_NAME = os.environ.get("USER_NAME", "")
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     DOWNLOAD_LOCATION = "./DOWNLOADS"
