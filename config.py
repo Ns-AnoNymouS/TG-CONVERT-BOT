6170147012619eafa697a6a5f3f1c5b53f3181c0
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-12 08:04:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-    BANNED_USERS = []
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
     OUO_IO_API_KEY = ""
