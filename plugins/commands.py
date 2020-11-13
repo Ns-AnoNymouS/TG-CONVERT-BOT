aa29d3086cc123ac1b8a09103357a396bcac30c9
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-11-13 20:24:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -9,8 +9,11 @@ from pyrogram import Client, Filters, InlineKeyboardButton, InlineKeyboardMarkup
 from translation import Translation
 from Tools.Download import download
 
+
+
 my_father = "https://t.me/{}".format(Config.USER_NAME[1:])
 support = "https://telegram.dog/Ns_Bot_supporters"
+
 @Client.on_message(Filters.command(["start"]))
 async def start(c, m):
 
@@ -42,6 +45,7 @@ async def about(c, m):
 
 @Client.on_message(Filters.command(["converttovideo"]))
 async def video(c, m):
+
   if Config.BOT_PWD:
       if m.from_user.id not in Conig.LOGGED_USER:
           await m.reply_text(text=Translation.NOT_LOGGED_TEXT, quote=True)
