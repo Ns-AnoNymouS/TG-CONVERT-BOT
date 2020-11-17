c134770753ee886493d9510eae51cb9666e8844c
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-11-17 20:29:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -75,7 +75,7 @@ async def file(c, m):
 async def login(c, m):
 
     if Config.BOT_PWD:
-        _, pass = m.text.split(" ")
+        com, pass = m.text.split(" ")
         if str(pass) == str(Config.BOT_PWD):
             await c.send_message(chat_id=m.chat.id,
                                  text=Translation.SUCESS_LOGIN,
