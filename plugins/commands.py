d40ea36c62a78f416ca673ecd4da2b2f38f4dfd3
123456789592 <77098458+123456789592@users.noreply.github.com>
2021-01-18 09:50:02 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -11,8 +11,8 @@ from Tools.Download import download
 
 
 
-my_father = "https://t.me/{}".format(Config.USER_NAME[1:])
-support = "https://telegram.dog/Ns_Bot_supporters"
+my_father = "https://t.me/Mega_Bots_Updates".format(Config.USER_NAME[1:])
+support = "https://t.me/Mega_Bots_Supporters"
 
 @Client.on_message(Filters.command(["start"]))
 async def start(c, m):
@@ -20,7 +20,7 @@ async def start(c, m):
     await c.send_message(chat_id=m.chat.id,
                          text=Translation.START.format(m.from_user.first_name, Config.USER_NAME),
                          reply_to_message_id=m.message_id,
-                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("My Father 👨‍💻", url=my_father), InlineKeyboardButton("📌Support channel", url=support)]]))
+                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("My Father 👨‍💻", url=https://t.me/Mega_Bots_Updates), InlineKeyboardButton("📌Support channel", url=https://t.me/Mega_Bots_Supporters)]]))
     logger.info(f"{m.from_user.first_name} used start command")
 
 
