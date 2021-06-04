0920eb109bea9c161064473f972f44af1f5aae12
Disneyteam <85325455+Disneyteam76@users.noreply.github.com>
2021-06-04 15:00:37 +0530

    Update

--- plugins/commands.py
+++ plugins/commands.py
@@ -12,7 +12,7 @@ from Tools.Download import download
 
 
 my_father = "https://t.me/{}".format(Config.USER_NAME[1:])
-support = "https://telegram.dog/Ns_Bot_supporters"
+support = "https://telegram.dog/disneyteamchat"
 
 @Client.on_message(Filters.command(["start"]))
 async def start(c, m):
